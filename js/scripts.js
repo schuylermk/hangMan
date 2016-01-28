657d5971ca69cc2087594573aecbd9cbe9bdecc6
Christine Tacha and Schuyler Klaassen <schuyler.m.k@gmail.com>
2016-01-28 09:55:46 -0800

    First spec pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
-function hangMan() {
-  var wordArray = ["dog", "cat", "soccer", "earth", "food"];
-  var word = wordArray[Math.floor(Math.random() * wordArray.length)];
+function HangMan() {
+  this.words = ["dog", "cat", "soccer", "earth", "food"];
+  // this.guessedLetters = guessedLetters;
+  // this.limbsLeft = 6;
+}
+
+HangMan.prototype.chooseWord = function() {
+  var word = this.words[Math.floor(Math.random() * this.words.length)];
   return word;
 };
 
+HangMan.prototype.splitWord = function(word) {
+  var arrayOne = word.split(["" [word.length]]);
+  return arrayOne;
+}
 
-// function myGame(currentWord, guessedLetters, limbsLeft) {
-//   this.currentWord = currentWord;
-//   this.guessedLetters = [];
-//   this.limbsLeft = 6;
+// for(var i = 0; i <= wordArray.length; i++) {
+//   if wor
 // }
+
 //
 // myGame.prototype.wordArray = function() {
 //   return this.guessedLetters === charAt(i);
@@ -21,7 +29,3 @@ function hangMan() {
 // var hangMan =  function()
 
 // var letters =
-
-// for(var i = 0; i <= wordArray.length; i++) {
-//   if wor
-// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,15 @@
 describe("hangMan", function() {
   it("will get a random word from an array", function() {
-    expect(hangMan("dog")).to.equal("dog");
+    var testHangMan = new HangMan();
+    expect(testHangMan.chooseWord()).to.be.a('string');
   });
-  it("will determine if a letter is in the word", function() {
-    expect(hangMan("a")).to.equal("a");
+  it("will split the words into letters", function() {
+    var testHangMan = new HangMan();
+    expect(testHangMan.splitWord()).to.eql(["c","a","t"]);
   });
 });
+
+
+// it("will determine if a letter is in the word", function() {
+//   expect(hangMan("a")).to.equal("a");
+// });
